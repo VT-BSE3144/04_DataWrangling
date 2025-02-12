32bff48ef0d0d088b69388e8cf93f16764ee8326
wrightrc <wright.clay@gmail.com>
2025-02-12 15:02:10 -0500

    add bookdown github workflow code

---
+++ .github/scripts/copy-intro-example-files.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# bash boilerplate
+set -euo pipefail # strict mode
+readonly SCRIPT_NAME="$(basename "$0")"
+readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+function l { # Log a message to the terminal.
+    echo
+    echo -e "[$SCRIPT_NAME] ${1:-}"
+}
+
+# Files to copy from Weekly Materials 
+INTRO_EXAMPLE_FILES=$(ls | grep -E '^[0-9]+-(0_Intro|0_intro|1_Examples)\.(Rmd|qmd)$')
+DIRECTORIES=$(ls | grep -E '(Data|data|Images|images|Figures|figures)')
+
+
+# if the file exists in Materials, copy it to Bookdown repo
+if [ -f "$INTRO_EXAMPLE_FILES" ]; then
+    for file in $INTRO_EXAMPLE_FILES; do
+        echo "Processing: $file"
+        cp -R $file $DESTINATION_PATH
+    done
+fi
+
+echo "Intro and Example files copied to $DESTINATION_PATH"
+
+# if the directory exists in Materials, copy it to Bookdown repo
+if [ -f "$DIRECTORIES" ]; then
+    for dir in $DIRECTORIES; do
+        echo "Processing: $dir"
+        cp -R $dir $DESTINATION_PATH
+    done
+fi
+
+echo "Directories copied to $DESTINATION_PATH"
