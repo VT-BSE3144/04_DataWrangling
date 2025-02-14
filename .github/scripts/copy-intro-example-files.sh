ddab5938c95fc316d0b872648eb67c73aa59a509
wrightrc <wright.clay@gmail.com>
2025-02-14 14:19:14 -0500

    simple but not generic path

--- .github/scripts/copy-intro-example-files.sh
+++ .github/scripts/copy-intro-example-files.sh
@@ -9,8 +9,8 @@ function l { # Log a message to the terminal.
 }
 
 # Files to copy from Weekly Materials 
-INTRO_EXAMPLE_FILES=$(ls ./source | grep -E '^[0-9]+-(0_Intro|0_intro|1_Examples)\.(Rmd|qmd)$')
-DIRECTORIES=$(ls ./source | grep -E '(Data|data|Images|images|Figures|figures)')
+INTRO_EXAMPLE_FILES=$(ls './04_DataWrangling' | grep -E '^[0-9]+-(0_Intro|0_intro|1_Examples)\.(Rmd|qmd)$')
+DIRECTORIES=$(ls './04_DataWrangling'  | grep -E '(Data|data|Images|images|Figures|figures)')
 
 
 # if the file exists in Materials, copy it to Bookdown repo
